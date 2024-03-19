b75f5208263d05aa576a99003021a7cffa85970b
Andrei <dedeigalinha@hotmail.com>
2024-03-19 16:51:49 -0300

    correcoes

--- Exercicio1.php
+++ Exercicio1.php
@@ -6,19 +6,19 @@ function NumPrimo($numero)
     if ($numero <= 1) {
         return false;
     }
-    //aqui é enquanto $i for menor igual a raiz do numero q for colocar, repete o loob e adicione mais um.
-    for ($i = 1; $i <= sqrt($numero); $i++) {
+    //aqui é enquanto $i for menor igual a raiz do numero q for colocar, repete o loop e adicione mais um.
+    for ($i = 2; $i <= sqrt($numero); $i++) {
         if ($numero % $i == 0) {
             return false;
         }
     }
     return true;
 }
-
+$numero = 997;
 // a funcao NumPrimo voltar true escreva é um numero primo. se nao...
 if (NumPrimo($numero)) {
     echo "é um numero primo";
 } else {
     echo "nao é um numero primo";
 }
-$numero = 0;  //coloque um numero aqui pra identificar se é ou nao é primo.
+//coloque um numero aqui pra identificar se é ou nao é primo.
